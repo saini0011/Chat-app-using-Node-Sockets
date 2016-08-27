75c7e39a5870a26c7b91187b33062bff95af41d6
Sidhant Saini <sidhantsaini01@gmail.com>
2016-08-27 12:23:45 -0400

    Add front end jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,22 @@
 var socket = io();
+
 socket.on('connect',function(){
-
-console.log('connected to socket.io server');
-
+  console.log('connected to socket.io server');
 });
 
 socket.on('message',function(message){
-	console.log('new message:',message.text);
+  console.log('new message:',message.text);
+});
+
+//handles submitting of new message
+
+var $form = jQuery('#message-form');
+$form.on('submit',function(event){
+  event.preventDefault();
+
+  var $message = $form.find('input[name=message]'); 
+  socket.emit('message',{
+   text: $message.val()
+  });
+   $message.val('');
 });
